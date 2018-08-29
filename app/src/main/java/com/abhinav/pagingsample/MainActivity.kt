1961094795f61c45bf9a85f545f760e589db27e9
abhinav272 <abhinav.ih@gmail.com>
2018-08-29 11:11:56 +0530

    flow init

--- app/src/main/java/com/abhinav/pagingsample/MainActivity.kt
+++ app/src/main/java/com/abhinav/pagingsample/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.abhinav.pagingsample
 
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.abhinav.pagingsample.ui.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    companion object {
+        private const val LAST_SEARCH_QUERY: String = "last_search_query"
+        private const val DEFAULT_QUERY = "Android"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViewModel()
+        setupDecorations()
+        setupScrollListner()
+        initAdapter()
+
+        val query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
+        viewModel.searchRepo(query)
+        initSearch(query)
+    }
+
+    private fun initSearch(query: String) {
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(LAST_SEARCH_QUERY, viewModel.lastQueryValue())
+    }
+
+    private fun initAdapter() {
+
+    }
+
+    private fun setupScrollListner() {
+
+    }
+
+    private fun setupDecorations() {
+
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 }
