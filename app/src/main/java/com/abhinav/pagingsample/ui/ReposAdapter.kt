0190fb25b7a33dffbe6584a75c3cbd5526dea01b
abhinav272 <abhinav.ih@gmail.com>
2018-09-02 15:24:19 +0530

    repo adapter created

---
+++ app/src/main/java/com/abhinav/pagingsample/ui/ReposAdapter.kt
@@ -0,0 +1,30 @@
+package com.abhinav.pagingsample.ui
+
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.util.DiffUtil
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.abhinav.pagingsample.data.model.RepoEntity
+
+class ReposAdapter : ListAdapter<RepoEntity, RecyclerView.ViewHolder>(REPO_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder
+            = RepoViewHolder.create(parent)
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val repoItem = getItem(position)
+        if (repoItem != null) {
+            (holder as RepoViewHolder).bind(repoItem)
+        }
+    }
+
+    companion object {
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<RepoEntity>() {
+            override fun areItemsTheSame(oldItem: RepoEntity, newItem: RepoEntity): Boolean =
+                    oldItem.fullName == newItem.fullName
+
+            override fun areContentsTheSame(oldItem: RepoEntity, newItem: RepoEntity): Boolean =
+                    oldItem == newItem
+        }
+    }
+}
