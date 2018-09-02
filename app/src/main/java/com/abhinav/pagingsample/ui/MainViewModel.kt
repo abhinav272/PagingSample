5172f67eeedd6861000f1f574721ed2f5578103c
abhinav272 <abhinav.ih@gmail.com>
2018-09-02 15:24:36 +0530

    view model refactor

--- app/src/main/java/com/abhinav/pagingsample/ui/MainViewModel.kt
+++ app/src/main/java/com/abhinav/pagingsample/ui/MainViewModel.kt
@@ -1,15 +1,45 @@
 package com.abhinav.pagingsample.ui
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
 import com.abhinav.pagingsample.data.GithubRepository
+import com.abhinav.pagingsample.data.model.RepoEntity
+import com.abhinav.pagingsample.data.model.RepoSearchResult
 
-class MainViewModel(repository: GithubRepository) : ViewModel() {
-    fun searchRepo(query: String) {
-
+class MainViewModel(private val repository: GithubRepository) : ViewModel() {
+    companion object {
+        private const val VISIBLE_THRESHOLD = 5
     }
 
-    fun lastQueryValue(): String? {
-        return null
+    private val queryLiveData = MutableLiveData<String>()
+    private val repoResult: LiveData<RepoSearchResult> = Transformations.map(queryLiveData) {
+        repository.searchQuery(it)
     }
+
+    val repos: LiveData<List<RepoEntity>> = Transformations.switchMap(repoResult) { it -> it.data }
+    val networkErrors: LiveData<String> = Transformations.switchMap(repoResult) { it -> it.networkErrors }
+
+    /**
+     * Search a repository based on a query string.
+     */
+    fun searchRepo(queryString: String) {
+        queryLiveData.postValue(queryString)
+    }
+
+    fun listScrolled(visibleItemCount: Int, lastVisibleItemPosition: Int, totalItemCount: Int) {
+        if (visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount) {
+            val immutableQuery = lastQueryValue()
+            if (immutableQuery != null) {
+                repository.requestMore(immutableQuery)
+            }
+        }
+    }
+
+    /**
+     * Get the last query value.
+     */
+    fun lastQueryValue(): String? = queryLiveData.value
 
 }
