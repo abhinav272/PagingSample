4c57a6be32cad5b341afd50423255a303139f5d3
abhinav272 <abhinav.ih@gmail.com>
2018-09-10 17:13:40 +0530

    service for news api created

---
+++ app/src/main/java/com/abhinav/pagingsample/data/api/NewsService.kt
@@ -0,0 +1,74 @@
+package com.abhinav.pagingsample.data.api
+
+import android.util.Log
+import com.abhinav.pagingsample.BuildConfig
+import com.abhinav.pagingsample.data.model.NewsItem
+import com.abhinav.pagingsample.data.model.NewsSearchResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+fun searchNews(query: String, onSuccess: (repos: List<NewsItem>) -> Unit,
+               onError: (error: String) -> Unit) {
+
+    NewsService.create().searchNewsForQuery(query).enqueue(object : Callback<NewsSearchResponse> {
+        override fun onFailure(call: Call<NewsSearchResponse>, t: Throwable) {
+            Log.e("NewsService: ", t.message)
+            onError(t.message ?: "unknown error")
+        }
+
+        override fun onResponse(call: Call<NewsSearchResponse>, response: Response<NewsSearchResponse>) {
+            if (response.isSuccessful
+                    && response.body() != null
+                    && response.body()!!.status.equals("ok")) {
+                response.body()!!.articles?.let { onSuccess(it) }
+            } else {
+                onError(response.errorBody()?.string() ?: "Unknown error")
+            }
+        }
+    })
+}
+
+
+interface NewsService {
+
+    @GET("/everything?")
+    fun searchNewsForQuery(@Query("q") query: String): Call<NewsSearchResponse>
+
+
+    companion object {
+        private const val BASE_URL = "https://newsapi.org/v2/"
+
+        fun create(): NewsService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                    .addInterceptor(logger)
+                    .addInterceptor { chain ->
+                        val originalReq = chain.request()
+                        val originalHttpUrl = originalReq.url()
+
+                        val newUrl = originalHttpUrl.newBuilder()
+                                .addQueryParameter("apiKey", BuildConfig.API_KEY).build()
+
+                        val reqst = originalReq.newBuilder().url(newUrl).build()
+
+                        chain.proceed(reqst)
+                    }
+                    .build()
+            return Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                    .create(NewsService::class.java)
+        }
+    }
+}
