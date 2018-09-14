2bac303c69ea25c67d98d913e1a74d16aae037ad
abhinav272 <abhinav.ih@gmail.com>
2018-09-14 17:11:51 +0530

    repo created for news fetching

--- app/src/main/java/com/abhinav/pagingsample/data/NewsRepository.kt
+++ app/src/main/java/com/abhinav/pagingsample/data/NewsRepository.kt
@@ -1,7 +1,31 @@
 package com.abhinav.pagingsample.data
 
-import com.abhinav.pagingsample.data.api.NewsService
+import android.arch.lifecycle.Transformations
+import android.arch.paging.LivePagedListBuilder
+import android.util.Log
+import com.abhinav.pagingsample.data.model.NewsSearchResult
 
-class NewsRepository(private val service: NewsService) {
+class NewsRepository {
+
+    companion object {
+        private val NETWORK_PAGE_SIZE = 20
+    }
+
+    fun searchNews(query: String): NewsSearchResult {
+        Log.d("NewsRepository", "New query: $query")
+
+        val dataSourceFactory = NewsDataSourceFactory(query)
+
+        val networkErrorsLiveData = Transformations.switchMap(dataSourceFactory.sourceLiveData) { input ->
+            input.networkState
+        }
+
+        val livePagedList = LivePagedListBuilder(dataSourceFactory, NETWORK_PAGE_SIZE)
+                .setInitialLoadKey(1)
+                .build()
+
+        return NewsSearchResult(livePagedList, networkErrorsLiveData)
+
+    }
 
 }
