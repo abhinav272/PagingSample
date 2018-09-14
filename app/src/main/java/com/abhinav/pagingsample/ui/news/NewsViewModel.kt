51c289c3ff0888d5235545901be693826eb0e78a
abhinav272 <abhinav.ih@gmail.com>
2018-09-14 17:10:58 +0530

    wiring livedata with Ui

--- app/src/main/java/com/abhinav/pagingsample/ui/news/NewsViewModel.kt
+++ app/src/main/java/com/abhinav/pagingsample/ui/news/NewsViewModel.kt
@@ -1,7 +1,27 @@
 package com.abhinav.pagingsample.ui.news
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
+import com.abhinav.pagingsample.data.NewsRepository
 
-class NewsViewModel:ViewModel() {
+class NewsViewModel : ViewModel() {
+
+    private val queryLiveData = MutableLiveData<String>()
+    private val newsSearchResult = Transformations.map(queryLiveData) {
+        NewsRepository().searchNews(it)
+    }
+
+    val newsSearchPagedData = Transformations.switchMap(newsSearchResult) {
+        it.data
+    }
+
+    val networkState = Transformations.switchMap(newsSearchResult) { input ->
+        input.networkErrors
+    }
+
+    fun searchNews(query: CharSequence) {
+        queryLiveData.value = query.toString()
+    }
 
 }
